cf87fd805275e497a3661273ba0b8c100105f32c
Philip Owino <jofonowino@gmail.com>
2021-09-06 08:44:39 +0300

    add readme file

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
@@ -3,6 +3,7 @@ package com.philipowino.medicalhealthtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BmiActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.heightEditText) EditText height;
     @BindView(R.id.weightEditText) EditText weight;
     @BindView(R.id.calculateButton) Button calculate;
@@ -25,6 +27,13 @@ public class BmiActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
+        if (view == calculate) {
+            Double heightDouble = Double.parseDouble(height.getText().toString());
+            Double weightDouble = Double.parseDouble(weight.getText().toString());
+            Double bmi =  weightDouble/Math.pow(heightDouble,2);
+            Log.d(TAG, "result");
+            Log.d(TAG, bmi.toString());
+        }
 
     }
 }

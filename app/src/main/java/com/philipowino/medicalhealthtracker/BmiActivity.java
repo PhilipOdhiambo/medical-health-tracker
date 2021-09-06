2f33c06507ad6c0695f26114f360a3ea60f48a54
Philip Owino <jofonowino@gmail.com>
2021-09-06 11:46:02 +0300

    prepare message for various states

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
@@ -37,6 +37,20 @@ public class BmiActivity extends AppCompatActivity implements View.OnClickListen
                 Double heightDouble = Double.parseDouble(height.getText().toString());
                 Double weightDouble = Double.parseDouble(weight.getText().toString());
                 Double bmi =  weightDouble/Math.pow(heightDouble,2);
+                String message;
+                if (bmi < 18.5 ) {
+                    message = "BMI of " + bmi.toString() + " : Under Weight (see a doctor)";
+                } else if (bmi >= 18.5 && bmi <= 24.9 ) {
+                    message = "BMI of " + bmi.toString() + " : Healthy Weight (Maintain Your Lifestyle)";
+                } else if (bmi >= 25 && bmi <= 29.9 ) {
+                    message = "BMI of " + bmi.toString() + " : Overweight (Check Your Lifestyle)";
+                } else if (bmi >= 30 && bmi <= 39.9 ) {
+                    message = "BMI of " + bmi.toString() + " : Obese (See a doctor)";
+                } else {
+                    message = "BMI of " + bmi.toString() + "? : Confirm Your Measurements";
+                }
+
+
             } catch (Exception exception) {
                 Toast.makeText(BmiActivity.this,"Empty input!",Toast.LENGTH_LONG).show();
             }

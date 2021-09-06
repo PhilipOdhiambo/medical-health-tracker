9ceece50fe09466ef655b1a46cecdeeb458db7fe
Philip Owino <jofonowino@gmail.com>
2021-09-06 07:57:31 +0300

    bind bmi vew to activity members

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
@@ -3,12 +3,28 @@ package com.philipowino.medicalhealthtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class BmiActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BmiActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.heightEditText) EditText height;
+    @BindView(R.id.weightEditText) EditText weight;
+    @BindView(R.id.calculateButton) Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        // Bind View
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }

43f0296a96db5b2ba50f93186fe10d2248324712
Philip Owino <jofonowino@gmail.com>
2021-09-06 11:21:39 +0300

    add toast to warn user empty fields

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
@@ -3,19 +3,20 @@ package com.philipowino.medicalhealthtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BmiActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.heightEditText) EditText height;
     @BindView(R.id.weightEditText) EditText weight;
-    @BindView(R.id.calculateButton) Button calculate;
+    @BindView(R.id.calculateButton) Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +24,22 @@ public class BmiActivity extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_bmi);
         // Bind View
         ButterKnife.bind(this);
+
+        // set onClick Listener
+        calculateButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        if (view == calculate) {
-            Double heightDouble = Double.parseDouble(height.getText().toString());
-            Double weightDouble = Double.parseDouble(weight.getText().toString());
-            Double bmi =  weightDouble/Math.pow(heightDouble,2);
-            Log.d(TAG, "result");
-            Log.d(TAG, bmi.toString());
+        if (view == calculateButton) {
+            try {
+                Double heightDouble = Double.parseDouble(height.getText().toString());
+                Double weightDouble = Double.parseDouble(weight.getText().toString());
+                Double bmi =  weightDouble/Math.pow(heightDouble,2);
+            } catch (Exception exception) {
+                Toast.makeText(BmiActivity.this,"Empty input!",Toast.LENGTH_LONG).show();
+            }
         }
 
     }

3211d0df93ae7fc1c2669acb2840a3c6114713cf
Philip Owino <jofonowino@gmail.com>
2021-09-06 11:51:46 +0300

    add activity to display bmi results

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiActivity.java
@@ -2,6 +2,7 @@ package com.philipowino.medicalhealthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.AndroidException;
 import android.util.Log;
---
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiResultActivity.java
@@ -0,0 +1,14 @@
+package com.philipowino.medicalhealthtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BmiResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi_result);
+    }
+}

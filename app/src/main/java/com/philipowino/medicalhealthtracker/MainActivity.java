6c20c2483c26c35eb10d21432200bbadef766a9e
Philip Owino <jofonowino@gmail.com>
2021-09-06 06:00:06 +0300

    add intent to move to activity_Bmi

--- app/src/main/java/com/philipowino/medicalhealthtracker/MainActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/MainActivity.java
@@ -2,12 +2,13 @@ package com.philipowino.medicalhealthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,12 +21,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         // Set listeners to views of interest on layout create time
+        ButterKnife.bind(this);
         mBMI.setOnClickListener(this);
         mEDD.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        if (view == mBMI) {
+            // Switch to BMI activity
+            Intent intent = new Intent(MainActivity.this,BmiActivity.class);
+            startActivity(intent);
+        }
 
     }
 }

ae09f4f6654d9a3c092ab026d1aeb0de4a3bf869
Philip Owino <jofonowino@gmail.com>
2021-09-06 04:12:59 +0300

    add Expected Delivery Date (EDD) activity and its .xml

---
+++ app/src/main/java/com/philipowino/medicalhealthtracker/EddActivity.java
@@ -0,0 +1,14 @@
+package com.philipowino.medicalhealthtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edd);
+    }
+}

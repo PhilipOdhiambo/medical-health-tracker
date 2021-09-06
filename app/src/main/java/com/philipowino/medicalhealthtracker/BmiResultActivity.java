869f03eba669629040862b58c8c02c78e0f65452
Philip Owino <jofonowino@gmail.com>
2021-09-06 12:15:51 +0300

    receive intent from activity bmi to activity_bmi_report

--- app/src/main/java/com/philipowino/medicalhealthtracker/BmiResultActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/BmiResultActivity.java
@@ -2,6 +2,7 @@ package com.philipowino.medicalhealthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BmiResultActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class BmiResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_result);
+        Intent intent = getIntent();
+        String weight = intent.getStringExtra("weight");
+        String height = intent.getStringExtra("height");
+        Double bmi = Double.parseDouble(intent.getStringExtra("bmi"));
     }
 }

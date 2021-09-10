58d892fa88650cf4094174804bc46944a92013dc
Philip Owino <jofonowino@gmail.com>
2021-09-10 08:57:34 +0300

    add AdverEventClient and api key and base_url

---
+++ app/src/main/java/com/philipowino/medicalhealthtracker/Constants.java
@@ -0,0 +1,7 @@
+package com.philipowino.medicalhealthtracker;
+
+public class Constants {
+    public static final String ADVERSE_EVENT_BASE_URL = "https://api.fda.gov/drug/event.json";
+    public static final String ADVERSE_EVENT_API_KEY = "WXpmEhw7Xcex8VZ6BUxs7e9N7EJr5r8yKBK1EBTz";
+
+}

3c5db3ed342625e4e12a6f6bf2d6e611678a6cce
PhilipOdhiambo <jofonowino@gmail.com>
2021-09-14 09:28:16 +0300

    refactor bmi section

--- app/src/main/java/com/philipowino/medicalhealthtracker/ui/BmiActivity.java
+++ app/src/main/java/com/philipowino/medicalhealthtracker/ui/BmiActivity.java
@@ -4,50 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.philipowino.medicalhealthtracker.R;
+import com.philipowino.medicalhealthtracker.databinding.ActivityBmiBinding;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BmiActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.heightEditText) EditText height;
-    @BindView(R.id.weightEditText) EditText weight;
-    @BindView(R.id.calculateButton) Button calculateButton;
+
+    // Implement view binding
+   private ActivityBmiBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bmi);
         // Bind View
-        ButterKnife.bind(this);
+        binding = ActivityBmiBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        // set onClick Listener
-        calculateButton.setOnClickListener(this);
+        // Add click listener
+        binding.calculateButton.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view == calculateButton) {
+        if (view == binding.calculateButton) {
+            Log.d("receive", binding.heightTextInputEditText.getText().toString());
             try {
-                Double heightDouble = Double.parseDouble(height.getText().toString());
-                Double weightDouble = Double.parseDouble(weight.getText().toString());
+                Double heightDouble = Double.parseDouble(binding.heightTextInputEditText.getText().toString());
+                Double weightDouble = Double.parseDouble(binding.weightEditText.getText().toString());
                 Double bmi =  weightDouble/Math.pow(heightDouble,2);
-
 
                 // Create intent
                 Intent intent = new Intent(BmiActivity.this,BmiResultActivity.class);
-                intent.putExtra("height", height.getText().toString());
-                intent.putExtra("weight", weight.getText().toString());
+                intent.putExtra("height", binding.heightTextInputEditText.getText().toString());
+                intent.putExtra("weight", binding.weightEditText.getText().toString());
                 intent.putExtra("bmi", String.format("%.2f",bmi));
                 startActivity(intent);
-
-
 
             } catch (Exception exception) {
                 Toast.makeText(BmiActivity.this,"Empty input!",Toast.LENGTH_LONG).show();
